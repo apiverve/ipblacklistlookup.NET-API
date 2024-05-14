55f8c7c208d54fc0877564853e1067560254ad8a
APIVerve Team <hello@apiverve.com>
2024-05-14 15:28:04 -0500

    Bug fixes and Improvements

---
+++ ipblacklistlookupQueryOptions.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APIVerve
+{
+    public class ipblacklistlookupQueryOptions {
+public string ip { get; set; }
+}
+
+}
