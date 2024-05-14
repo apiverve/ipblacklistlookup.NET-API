c4ca36fbc2f93df9cd9c3b9ad6bc4d53835f78dd
APIVerve Team <hello@apiverve.com>
2024-05-14 15:28:06 -0500

    Bug fixes and Improvements

--- ResponseObj.cs
+++ ResponseObj.cs
@@ -1,21 +1,51 @@
 using System;
 using System.Collections.Generic;
-using System.Text;
+using Newtonsoft.Json;
 
 namespace APIVerve
 {
-    using Newtonsoft.Json;
+public class ipDetails
+{
+    [JsonProperty("range")]
+    public int[] range { get; set; }
+
+    [JsonProperty("country")]
+    public string country { get; set; }
+
+    [JsonProperty("region")]
+    public string region { get; set; }
+
+    [JsonProperty("timezone")]
+    public string timezone { get; set; }
+
+}
+
+public class data
+{
+    [JsonProperty("ipAddress")]
+    public string ipAddress { get; set; }
+
+    [JsonProperty("isIPBlacklisted")]
+    public bool isIPBlacklisted { get; set; }
+
+    [JsonProperty("ipDetails")]
+    public ipDetails ipDetails { get; set; }
+
+    [JsonProperty("parsed")]
+    public bool parsed { get; set; }
+
+}
 
 public class ResponseObj
 {
     [JsonProperty("status")]
-    public string Status { get; set; }
+    public string status { get; set; }
 
     [JsonProperty("error")]
-    public string Error { get; set; }
+    public object error { get; set; }
 
     [JsonProperty("data")]
-    public object Data { get; set; }
+    public data data { get; set; }
 
 }
 
